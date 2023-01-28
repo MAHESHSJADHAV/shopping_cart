202b1f8a7ed1f11468551eebce9e520696c78e7a
MAHESHSJADHAV <97753228+MAHESHSJADHAV@users.noreply.github.com>
2023-01-28 11:03:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+              </>
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <>
+                <Cart />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+//import ReactDOM from 'react-dom';
+// import './index.css';
+import App from "./App";
+import Context from "./Context/Context";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Context>
+      <App />
+    </Context>
+  </React.StrictMode>
+);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Context>
+//     <App />
+//     </Context>
+
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
